20429483299547aa0f9c007193ddc394c20f8148
Abrahamkivosh <abrahamkivosh@gmail.com>
2025-02-11 11:30:30 +0300

    s

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,47 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>{{ config('app.name', 'Ticket Kenya') }}</title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no'
+        name='viewport' />
+    <link rel="stylesheet" href="{{ asset('admin_assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+    <link rel="stylesheet" href="{{ asset('admin_assets/css/ready.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin_assets/css/demo.css') }}">
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.ts'])
+    @yield('styles')
+</head>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+<body>
+    <div class="wrapper">
+        @include('includes.admin.main-header')
+        @include('includes.admin.mainSidebar')
+        {{-- Main content --}}
+        @yield('content')
+    </div>
+    </div>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+</body>
+<script src="{{ asset('admin_assets/js/core/jquery.3.2.1.min.js') }}"></script>
+<script src="{{ asset('admin_assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js') }}"></script>
+<script src="{{ asset('admin_assets/js/core/popper.min.js') }}"></script>
+<script src="{{ asset('admin_assets/js/core/bootstrap.min.js') }}"></script>
+<script src="{{ asset('admin_assets/js/plugin/chartist/chartist.min.js') }}"></script>
+<script src="{{ asset('admin_assets/js/plugin/chartist/plugin/chartist-plugin-tooltip.min.js') }}"></script>
+<script src="{{ asset('admin_assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js') }}"></script>
+<script src="{{ asset('admin_assets/js/plugin/bootstrap-toggle/bootstrap-toggle.min.js') }}"></script>
+<script src="{{ asset('admin_assets/js/plugin/jquery-mapael/jquery.mapael.min.js') }}"></script>
+<script src="{{ asset('admin_assets/js/plugin/jquery-mapael/maps/world_countries.min.js') }}"></script>
+<script src="{{ asset('admin_assets/js/plugin/chart-circle/circles.min.js') }}"></script>
+<script src="{{ asset('admin_assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js') }}"></script>
+<script src="{{ asset('admin_assets/js/ready.min.js') }}"></script>
+<script src="{{ asset('admin_assets/js/demo.js') }}"></script>
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+@yield('scripts')
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
 </html>
