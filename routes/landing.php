8906378c84d5912506f65e29d3eb317564aef7f8
Abrahamkivosh <abrahamkivosh@gmail.com>
2025-02-13 17:41:20 +0300

    d

--- routes/landing.php
+++ routes/landing.php
@@ -7,3 +7,4 @@ Route::get('/', [PageController::class, 'indexPage']);
 Route::get('/contact-us', [PageController::class, 'contactPage']);
 Route::get('/all-events/{event}', [PageController::class, 'singleEvent'])->name('event.single');
 Route::get('/event/{event}/ticket/{ticket}', [PageController::class, 'eventTicketBuy'])->name('event.ticket.buy');
+Route::post('/event/{event}/ticket/{ticket}', [PageController::class, 'purchase'])->name('event.ticket.purchase');
